787b0cba643784df25047a5651d7fa9881f3d5ab
xoposhiy@mail.ru <xoposhiy@mail.ru@ad59adf0-0fde-11de-af30-3d3b2b6d83b0>
2009-03-14 21:40:23 +0000

    Заготовка для нашего всепобеждающего бота.

---
+++ tools/Sapka/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TheSapka
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string host = args.Length > 0 ? args[0] : "localhost";
+			int port = args.Length > 1 ? int.Parse(args[1]) : 20015;
+			string teamName = args.Length > 2 ? args[2] : "ural-sapkers";
+			new Sapka(host, port, teamName).Run();
+		}
+	}
+}
