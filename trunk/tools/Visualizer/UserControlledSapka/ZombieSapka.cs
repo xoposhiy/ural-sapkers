94bd9a48887d1a9fb122c320f2212cfb9784ac72
xoposhiy@mail.ru <xoposhiy@mail.ru@ad59adf0-0fde-11de-af30-3d3b2b6d83b0>
2009-03-15 10:46:24 +0000

    Zomby -> Zombie

---
+++ trunk/tools/Visualizer/UserControlledSapka/ZombieSapka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+
+namespace Visualizer.UserControlledSapka
+{
+	class ZombieSapka : AbstractSapka
+	{
+		private readonly IZombieMaster zombieMaster;
+
+		public ZombieSapka(string host, int port, string teamName, IZombieMaster zombieMaster) : base(host, port, teamName)
+		{
+			this.zombieMaster = zombieMaster;
+		}
+
+		public override string GetMove()
+		{
+			string s = zombieMaster.Command.ToString();
+			if(zombieMaster.IsBombing()) s += 'b';
+			return s;
+		}
+	}
+}
