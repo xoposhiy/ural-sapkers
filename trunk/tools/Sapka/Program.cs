296af7e0eb0652029aa3d8b101c2064bdf248b61
Andrew.Kostousov@gmail.com <Andrew.Kostousov@gmail.com@ad59adf0-0fde-11de-af30-3d3b2b6d83b0>
2009-03-17 19:09:12 +0000

    Добавил каркас для подготовки сабмита. + дистрибутив VNC-клиента для подключения терминалом к Sapka LiveCD.

--- trunk/tools/Sapka/Program.cs
+++ trunk/tools/Sapka/Program.cs
@@ -12,6 +12,7 @@ namespace TheSapka
 			string host = args.Length > 0 ? args[0] : "localhost";
 			int port = args.Length > 1 ? int.Parse(args[1]) : 20015;
 			string strategy = args.Length > 2 ? args[2] : "";
+			System.Console.WriteLine("Launched: Sapka.exe {0} {1} {2}", host, port, strategy);
 			GetSapka(host, port, strategy).Run();
 		}
 
