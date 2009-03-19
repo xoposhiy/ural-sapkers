e0f85fa54f32992e6f38dbff38a5137e6a58da71
Ivan.Komarov@dfyz.info <Ivan.Komarov@dfyz.info@ad59adf0-0fde-11de-af30-3d3b2b6d83b0>
2009-03-19 22:56:13 +0000

    * Убрал мгновенное фото нафиг. Теперь мысли сапки можно читать в режиме просмотра логов на паузе. * Пофиксил хождение через стены при показе мыслей.

---
+++ trunk/tools/Visualizer/DecisionVisualizer.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using System.Drawing;
+using Core.Parsing;
+using Core.PathFinding;
+using Core.StateCalculations;
+
+namespace Visualizer
+{
+	public class DecisionVisualizer
+	{
+		public string Draw(Graphics gr, GameState state, int pointSize, int cellSize, int paddingX, int paddingY)
+		{
+			pathFinder.SetMap(state.Map, state.CellSize);
+			DrawSelectedSapka(gr, pointSize, paddingX, paddingY);
+			return DrawSelectedPath(gr, state.Time, pointSize, cellSize, paddingX, paddingY, state.Map);
+		}
+
+		public void SelectStartPoint(int pointX, int pointY, int speed)
+		{
+			selectedStartPos = new Pos(pointX, pointY);
+			selectedSpeed = speed;
+		}
+
+		public void SelectEndPoint(int pointX, int pointY)
+		{
+			selectedEndPos = new Pos(pointX, pointY);
+		}
+
+		private void DrawSelectedSapka(Graphics gr, int pointSize, int paddingX, int paddingY)
+		{
+			if (selectedStartPos == null) return;
+			Fill(gr, selectedStartPos.X*pointSize, selectedStartPos.Y*pointSize, true, pointSize, paddingX, paddingY);
+		}
+
+		private string DrawSelectedPath(Graphics gr, int time, int pointSize, int cellSize, int paddingX, int paddingY, MapCell[,] cells)
+		{
+			if (selectedStartPos == null) return "Выберите начальную точку";
+			if (selectedEndPos == null) return "Выберите конечную точку";
+			var paths = pathFinder.FindPaths(selectedStartPos.X, selectedStartPos.Y, time, selectedSpeed, int.MaxValue);
+			var alive = pathFinder.Live(selectedStartPos.X, selectedStartPos.Y, time, selectedSpeed);
+			if (selectedEndPos.X >= paths.GetLength(0) || selectedEndPos.Y >= paths.GetLength(1)) return "Вылетели за пределы поля";
+			var path = paths[selectedEndPos.X, selectedEndPos.Y];
+			if (path == null) return "Уже таки прекратите тыкать куда попало!";
+			var fullPath = new string(path.FullPath().ToArray());
+			var curPixelX = selectedStartPos.X * pointSize;
+			var curPixelY = selectedStartPos.Y * pointSize;
+			foreach (var dir in fullPath)
+			{
+				if (dir == 's')
+				{
+					continue;
+				}
+				for (int i = 0; i < selectedSpeed; i++)
+				{
+					Fill(gr, curPixelX, curPixelY, alive, pointSize, paddingX, paddingY);
+					int nextPixelX = curPixelX + dx[dir] * pointSize;
+					int nextPixelY = curPixelY + dy[dir] * pointSize;
+					var nextCell = cells[nextPixelX/cellSize, nextPixelY/cellSize];
+					if (nextCell.IsBreakableWall || nextCell.IsUnbreakableWall) continue;
+					curPixelX = nextPixelX;
+					curPixelY = nextPixelY;
+				}
+			}
+			Fill(gr, curPixelX, curPixelY, alive, pointSize, paddingX, paddingY);
+			return fullPath;
+		}
+
+		private static void Fill(Graphics gr, int x, int y, bool alive, int pointSize, int paddingX, int paddingY)
+		{
+			gr.FillRectangle(alive ? Brushes.Yellow : Brushes.Cyan, x + paddingX, y + paddingY, pointSize, pointSize);
+		}
+
+		private Pos selectedStartPos;
+		private Pos selectedEndPos;
+		private int selectedSpeed;
+		private readonly IPathFinder pathFinder = new PathFinder();
+
+		private readonly IDictionary<char, int> dx = new Dictionary<char, int>
+			{
+				{ 'u', 0 },
+				{ 'd', 0 },
+				{ 'l', -1 },
+				{ 'r', 1 }
+			};
+
+		private readonly IDictionary<char, int> dy = new Dictionary<char, int>
+			{
+				{ 'u', -1 },
+				{ 'd', 1 },
+				{ 'l', 0 },
+				{ 'r', 0 }
+			};
+	}
+}
